f0c85ba4c894ff6ea9319a799c9e0120fa6414d6
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-07-12 21:31:02 +0600

    app.ts folder delete

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,32 @@
 import mongoose from "mongoose";
-import app from "./app";
+import express, { Express, NextFunction, Request, Response } from "express";
+import { errorHandlerMiddleware } from "./middleware/error-handler";
+import userRouter from "./modules/user/user.route";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import cowRouters from "./modules/cow/cow.route";
+import "dotenv/config";
+import ordersRouters from "./modules/orders/order.route";
 
 const port: number = 1000;
 const uri: any = process.env.DB_URL || "mongodb://localhost:27017/firstdb";
+
+const app: Express = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(errorHandlerMiddleware);
+
+app.use(userRouter);
+app.use(cowRouters);
+app.use(ordersRouters);
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello Cows Backend");
+});
 
 app.listen(port, async () => {
   try {
